9a640794c1e2ca10e856e8836e1e61563c9b54be
Dayana306 <pradoalison74@gmail.com>
2026-07-24 06:16:55 -0600

    Delete FlashBite1.0 directory

--- FlashBite1.0/php/vaciar_carrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION["carrito"])) {
-    unset($_SESSION["carrito"]);
-}
-
-header("Location: ../form_pedido.php");
-exit();
--- FlashBite1.0/prueba.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include("conexion/conexion.php");
-
-echo "Conexión realizada correctamente.";
-
-?>
